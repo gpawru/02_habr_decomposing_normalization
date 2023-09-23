bb34b8094b8ab47092ca9bafa52e2815ce4d154d
Anton <anton@gpaw.ru>
2023-09-23 21:54:17 +0300

    ..

---
+++ tests/src/ucd.rs
@@ -0,0 +1,71 @@
+use unicode_normalization_source::{NormalizationTest, NORMALIZATION_TESTS};
+
+use unicode_decomposing_v1::DecomposingNormalizer as v1;
+use unicode_decomposing_v2::DecomposingNormalizer as v2;
+
+macro_rules! test {
+    ($left: expr, $right: expr, $normalizer: expr, $test: expr, $str: expr) => {
+        assert_eq!(
+            $left,
+            $normalizer.normalize(&$right),
+            stringify!($str),
+            $test.line,
+            $test.description
+        );
+    };
+}
+
+/// тесты NFD нормализации из UCD
+#[test]
+fn ucd_test_nfd()
+{
+    // c3 ==  toNFD(c1) ==  toNFD(c2) ==  toNFD(c3)
+    // c5 ==  toNFD(c4) ==  toNFD(c5)
+
+    let tests: &Vec<NormalizationTest> = &NORMALIZATION_TESTS;
+
+    macro_rules! test_group {
+        ($($normalizer: expr),+) => {
+            $(
+                let normalizer = $normalizer;
+
+                for t in tests {
+                    test!(t.c3, t.c1, normalizer, t, "{} {}: c3 == toNFD(c1)");
+                    test!(t.c3, t.c2, normalizer, t, "{} {}: c3 == toNFD(c2)");
+                    test!(t.c3, t.c3, normalizer, t, "{} {}: c3 == toNFD(c3)");
+                    test!(t.c5, t.c4, normalizer, t, "{} {}: c5 == toNFD(c4)");
+                    test!(t.c5, t.c5, normalizer, t, "{} {}: c5 == toNFD(c5)");
+                }
+            )+
+        };
+    }
+
+    test_group!(v1::nfd(), v2::nfd());
+}
+
+/// тесты NFKD нормализации из UCD
+#[test]
+fn ucd_test_nfkd()
+{
+    // c5 == toNFKD(c1) == toNFKD(c2) == toNFKD(c3) == toNFKD(c4) == toNFKD(c5)
+
+    let tests: &Vec<NormalizationTest> = &NORMALIZATION_TESTS;
+
+    macro_rules! test_group {
+        ($($normalizer: expr),+) => {
+            $(
+            let normalizer = $normalizer;
+
+            for t in tests {
+                test!(t.c5, t.c1, normalizer, t, "{} {}: c5 == toNFKD(c1)");
+                test!(t.c5, t.c2, normalizer, t, "{} {}: c5 == toNFKD(c2)");
+                test!(t.c5, t.c3, normalizer, t, "{} {}: c5 == toNFKD(c3)");
+                test!(t.c5, t.c4, normalizer, t, "{} {}: c5 == toNFKD(c4)");
+                test!(t.c5, t.c5, normalizer, t, "{} {}: c5 == toNFKD(c5)");
+            }
+        )+
+        };
+    }
+
+    test_group!(v1::nfkd(), v2::nfkd());
+}
